0824f3cea18fcb50c942106bc688f3ba1d0229d1
wwh <847666794www@163.com>
2024-10-12 15:05:02 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <Menu></Menu>
+</template>
+
+<script setup>
+  import Menu from '@/views/Menu/index.vue'
+</script>
+
+<style scoped>
+</style>
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+
+// 引入resolve
+import { resolve } from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),
+  ],
+  resolve: {
+    // 设置路径别名
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "*": resolve(""),
+    },
+  },
+});

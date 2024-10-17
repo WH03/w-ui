396e22ceb3401b4ed759c5ab9173942ccce261b6
wwh <847666794www@163.com>
2024-10-17 17:34:00 +0800

    引入pinia

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import "@/style/style.scss";
 import App from "./App.vue";
 import router from "./router/index.js";
+import store from "./store";
+const app = createApp(App);
 
-createApp(App).use(router).mount("#app");
+app.use(router);
+app.use(store);
+app.mount("#app");
